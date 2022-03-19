b567f458302f3ada9d88ed8799217071d1a9d815
flpdsc <92yjkim@gmail.com>
2022-03-19 16:23:40 +0900

    0318

---
+++ 01_cs_basic/day02/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace day02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region c#의 자료형
+
+            if (false)
+            {
+                Console.WriteLine("Hello World!");
+
+                int number = 5000;
+                float weight = 50.5f;
+                double pi = 3.1415;
+                char word = 'a';
+                string name = "AAA";
+
+                // object 자료형 : c#의 모든 자료형이 상속하고 있는 자료형. (최상위 자료형?)
+                // 그렇기 때문에 어떠한 자료형의 값이라도 대입받을 수 있다.
+                // 박싱 : boxing
+                // => 대입한 메모리를 감싸서 Heap에 올려두고 참조하는 것.
+                object box = number;
+
+                // castring (=>자료형 변환)
+                // 언박싱 : unboxing
+                // => 오브젝트가 감싸고 있던 메모리를 다시 원래대로 되돌려주는 것.
+                int score = (int)box;
+
+                string text = number.ToString("#,##0");
+
+                Console.WriteLine(text);
+
+                text = weight.ToString();
+                text = pi.ToString();
+                text = word.ToString();
+
+                // Equals는 object 클래스에 존재하는 함수다.
+                // 따라서 어떠한 자료형도 호출할 수 있고 매게변수를 object로 받기 때문에
+                // 역시나 어떠한 자료형도 받아와서 비교할 수 있다.
+                if (!number.Equals(name))
+                {
+                    // WriteLine도 오버로딩된 함수의 매게변수 혹은 object로 변수를 받아와
+                    // ToString() 함수를 호출해 문자열을 출력한다.
+                    Console.WriteLine(name);
+                }
+            }
+
+            #endregion
+
+            #region string과 문자열
+
+            int aaa = 100;
+            int bbb = 200;
+
+            // 문자열 상수는 immutable type이다. (불변하는 값)
+            // string 변수는 mutable type이다. (불변하지 않는 값)
+            // string은 문자열을 참조한다. (문자열이 아니다.)
+            string job = "warrior";
+            job = "archor";
+            for(int i = 0; i<job.Length; i++)
+                Console.WriteLine("{0} : {1}", i, job[i]);
+            
+            // 따라서 job[0]에다 'a'를 대입하는 행위는 허용되지 않는다.
+            // 불변하는 값인 문자열 상수에 접근했기 때문.
+
+            // 출력문
+            // 1번째 매게변수는 문자열 서식
+            // 2번째 이상부터는 서식에 들어갈 값들.
+            // {0}은 0번째 값, {1}은 1번째 값.
+            Console.WriteLine("AAA:{1}, BBB:{0},{0}", aaa, bbb);
+            Console.WriteLine($"AAA:{aaa}, BBB:{bbb}");
+
+            #endregion
+
+            // 비밀번호 :
+            // 비밀번호 확인 :
+            // 두 값이 동일하면 '비밀번호가 동일합니다.'
+            // 두 값이 다르면 '비밀번호가 다릅니다.' 출력            
+            string password;
+            string passwordRe;
+
+            Console.Write("비밀번호 : ");
+            password = Console.ReadLine();
+
+            Console.Write("비밀번호 확인 : ");
+            passwordRe = Console.ReadLine();
+
+            if(password == passwordRe)
+            {
+                Console.WriteLine("비밀번호가 동일합니다.");
+            }
+            else
+            {
+                Console.WriteLine("비밀번호가 다릅니다.");
+            }
+        }
+    }
+}
---
+++ 01_cs_basic/day03/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+namespace cs03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (false) //Console.Read();
+            {
+                int read = Console.Read();
+                read = Console.Read();
+                read = Console.Read();
+                //abc 입력
+                Console.WriteLine(read); //99
+            }
+
+            if (false) //Console.ReadKey();
+            {
+                ConsoleKeyInfo keyinfo = Console.ReadKey(false); //false : 입력키 안 나옴
+                Console.WriteLine("Key:{0}", keyinfo.Key);
+                Console.WriteLine($"KeyChar:{keyinfo.KeyChar}");
+                Console.WriteLine($"Modifiers:{keyinfo.Modifiers}");
+            }
+
+            if (false) //Split, Replace, Substring
+            {
+                //Split
+                string list = "AAA,BBB,CCC,DDD";
+                string[] names = list.Split(',');
+                for(int i=0; i<names.Length; ++i)
+                {
+                    Console.WriteLine($"{i}번째 값은 : {names[i]}");
+                }
+
+                //Replace
+                string someText = "My name is cat";
+                someText = someText.Replace("cat", "bird");
+                Console.WriteLine($"someText : {someText}");
+
+                //Substring
+                string stringEx = "Today is Friday";
+                stringEx = stringEx.Substring(3);
+                Console.WriteLine(stringEx); //ay is Friday
+
+                string stringEx2 = "Today is Friday";
+                stringEx2 = stringEx2.Substring(6, 2);
+                Console.WriteLine(stringEx2); //is
+            }
+
+            if (true) //실습: 회원가입
+            {
+                string id = string.Empty;
+                string pw, pwc;
+
+                while (true)
+                {
+                    Console.Clear();
+                    pw = string.Empty;
+                    pwc = string.Empty;
+
+                    Console.Write("ID : ");
+                    if (string.IsNullOrEmpty(id))
+                    {
+                        id = Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine(id);
+                    }
+
+                    Console.Write("PW : ");
+                    pw = InputPW();
+
+                    if (!IsRange(pw, 4, 20))
+                    {
+                        Console.WriteLine("비밀번호의 길이는 최소 4자, 최대 20자 입니다.");
+                        Console.ReadKey();
+                        continue;
+                    }
+
+                    Console.Write("PW Confirm : ");
+                    pwc = InputPW();
+
+                    if (pw == pwc)
+                    {
+                        Console.WriteLine("비밀번호 같음");
+                    }
+                    else
+                    {
+                        Console.WriteLine("비밀번호 다름");
+                    }
+                    Console.ReadKey();
+                }
+            }
+        }
+
+
+        static bool IsRange(string str, int min, int max)
+        {
+            return str.Length >= min && str.Length <= max;
+        }
+
+        static string InputPW()
+        {
+            string inp = string.Empty;
+            while (true)
+            {
+                ConsoleKeyInfo info = Console.ReadKey(true);
+                if (info.Key == ConsoleKey.Enter)
+                {
+                    Console.WriteLine();
+                    break;
+                }
+                Console.Write("*");
+                inp += info.KeyChar;
+            }
+            return inp;
+        }
+    }
+}
