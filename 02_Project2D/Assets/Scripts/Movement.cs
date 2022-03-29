d1ee91b50d270ce1eb8cf9c370f222119f3f7169
flpdsc <92yjkim@gmail.com>
2022-03-30 00:18:42 +0900

    0329

--- 02_Project2D/Assets/Scripts/Movement.cs
+++ 02_Project2D/Assets/Scripts/Movement.cs
@@ -6,21 +6,20 @@ public class Movement : MonoBehaviour
 {
 
     [SerializeField] float speed = 5f;
+    [SerializeField] Animator anim;
+    [SerializeField] SpriteRenderer spriteRenderer;
 
     private void Start()
     {
-        Vector3 position = transform.position;
-        transform.position = new Vector3(3.0f, 2.0f, 0.0f);
-        transform.Translate(new Vector3(10.0f, 0.0f, 0.0f));
-        Debug.Log(position);
     }
 
     private void Update()
     {
-        float x = Input.GetAxisRaw("Horizontal");
-        float y = Input.GetAxisRaw("Vertical");
-
+        int x = (int)Input.GetAxisRaw("Horizontal");
         transform.Translate(Vector3.right * x * speed * Time.deltaTime);
-        transform.Translate(Vector3.up * y * speed * Time.deltaTime);
+
+        anim.SetInteger("horizontal", x);
+        if (x != 0)
+            spriteRenderer.flipX = (x == -1);
     }
 }
